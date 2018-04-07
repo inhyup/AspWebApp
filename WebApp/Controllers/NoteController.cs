888acca5dcb84017f72173a7b7a0206b5229bd6f
Inhyup Kim <hyup3569@gmail.com>
2018-04-06 22:11:21 -0600

    add note page

---
+++ WebApp/Controllers/NoteController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.DataContext;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class NoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            using (var db = new WebAppDbContext())
+            {
+                var list = db.Notes.ToList();
+                return View(list);
+            }
+        }
+
+        public IActionResult Add()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Note model)
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            model.UserNo = int.Parse(HttpContext.Session.GetInt32("USER_LOGIN_KEY").ToString());
+
+            if (ModelState.IsValid)
+            {
+                using (var db = new WebAppDbContext())
+                {
+                    db.Notes.Add(model);
+                    if (db.SaveChanges() > 0)
+                    {
+                        return Redirect("Index");
+                    }
+                }
+                ModelState.AddModelError(string.Empty, "Can't save your note");
+            }
+            return View(model);
+        }
+
+        public IActionResult Edit()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+    }
+}
